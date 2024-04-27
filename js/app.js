b828abe4d828bd5d065283640e9367e03f9c6fd6
Omar Alexis Torbisco Pizarro <omar16354@gmail.com>
2024-04-26 21:09:31 -0500

    fix href index y sales

--- js/app.js
+++ js/app.js
@@ -43,7 +43,7 @@ locales.forEach(ubicacion => {estimateSales(ubicacion);});
 
 const mainElement = document.querySelector('main');
 
-if(window.location.pathname==='sales.html'){
+if(window.location.pathname==='/sales.html'){
     locales.forEach(ubicacion => {
         const locationList = document.createElement('ul');
         const locationHeader = document.createElement('h2');
@@ -60,7 +60,7 @@ if(window.location.pathname==='sales.html'){
         }
         mainElement.appendChild(locationList);
     });
-}else if(window.location.pathname === 'index.html'){
+}else if(window.location.pathname === '/index.html'){
     function createLocationInfo(ciudad, horario, telefono, direccion) {
         return {
             ciudad: ciudad,
